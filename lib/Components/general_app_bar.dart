4e9afe52846b6e803387b9033b817237910050d3
AJ <aj.avinav@gmail.com>
2021-06-20 17:21:30 +0530

    Assets, appbar design added

---
+++ lib/Components/general_app_bar.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:medicure/constants.dart';
+
+class GeneralAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: kAppBarPadding,
+      child: AppBar(
+        toolbarHeight: kAppBarHeight,
+        backgroundColor: kBackgroundColor,
+        elevation: 0,
+        title: Text(
+          'Hello, Alison',
+          style: kAppBarTitleTextStyle,
+        ),
+        leading: Padding(
+          padding: kAppBarButtonPadding,
+          child: ElevatedButton(
+            onPressed: () {},
+            child: Image.asset('images/user.png'),
+            style: ElevatedButton.styleFrom(
+                primary: kAccountButtonBgColor,
+                shape: CircleBorder(),
+                elevation: 5.0,
+                side: kAccountBorder,
+            )
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: kAppBarButtonPadding,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Icon(Icons.search, color: Colors.black54),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                primary: kSearchButtonColor,
+                elevation: 5.0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+//General Theme
+const kBackgroundColor = Color(0xFEF3FBFE);
+
+//AppBar related constants
+const kAppBarPadding = EdgeInsets.only(top: 20.0, left: 12.0, right: 12.0);
+const kAppBarHeight = 80.0;
+const kAppBarTitleTextStyle = TextStyle(
+    fontFamily: 'Montserrat',
+    color: Colors.black,
+    fontSize: 20.0
+);
+const kAppBarButtonPadding = EdgeInsets.all(5.0);
+const kAccountButtonBgColor = Color(0xCCE0E0E0);
+const kAccountBorder = BorderSide(width: 2.5, color: Color(0xFF66E9E9));
+const kSearchButtonColor = Color(0xCC66E9E9);
