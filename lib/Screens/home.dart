49b55e213a09adf18ae49cd25266b6891e27b027
AJ <aj.avinav@gmail.com>
2021-06-21 01:19:19 +0530

    Home Screen design added

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:medicure/constants.dart';
 import 'package:medicure/Components/general_app_bar.dart';
+import 'package:medicure/Components/medicine_card.dart';
 
 class HomePage extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +14,59 @@ class HomePage extends StatelessWidget {
         preferredSize: Size.fromHeight(kAppBarHeight),
         child: GeneralAppBar(),
         ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: kIntroTextPadding,
+            child: Text(
+              'It\'s time for\nYour Medicines',
+              style: kIntroTextStyle,
+            ),
+          ),
+          MedicineCard(toggle: true),
+          MedicineCard(toggle: true),
+          MedicineCard(toggle: false),
+          MedicineCard(toggle: true),
+          MedicineCard(toggle: true)
+        ],
+      ),
+      bottomNavigationBar: Theme(
+        data: ThemeData(
+          backgroundColor: kBackgroundColor,
+        ),
+        child: BottomNavigationBar(
+          elevation: 5.0,
+
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.calendar),
+              label: 'Appointments',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset('images/parental-control.png', height: 25),
+              label: 'Parental',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.map),
+              label: 'Nearby',
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: kSelectedItemColor,
+          unselectedItemColor: kUnselectedItemColor,
+          showUnselectedLabels: true,
+          unselectedLabelStyle: kUnselectedTextStyle,
+          onTap: (int index){},
+        ),
+      ),
     );
   }
 }
