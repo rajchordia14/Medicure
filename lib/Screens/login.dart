89cbe0b7917a8285137df706bc65650550b0c0b9
GauravS1910 <19ucs246@lnmiit.ac.in>
2021-06-23 00:39:28 +0530

    edit loginPage

---
+++ lib/Screens/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:medicure/constants.dart';
+import 'package:medicure/Components/general_app_bar.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+
+      ),
+    );
+  }
+}
